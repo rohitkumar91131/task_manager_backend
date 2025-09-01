f4187eff3e8b2c025c5d320a208a5ea75fbfbf19
rohitkumar91131 <rk34190100@gmail.com>
2025-09-01 18:46:21 +0530

    added edit mode

--- Routes/TaskRoute.js
+++ Routes/TaskRoute.js
@@ -50,6 +50,46 @@ router.post("/new_task",verifyToken,async(req, res)=>{
     }
 })
 
+router.patch("/edit_task", verifyToken, async (req, res) => {
+    try {
+      const { task_id, content } = req.body;
+  
+      if (!task_id || !content || !content.trim()) {
+        return res.json({ 
+            success: false, 
+            msg: "Task ID and content are required" 
+        });
+      }
+  
+      const task = await Task.findOne({ 
+        _id: task_id, 
+        user_id: req.user.user_id
+      });
+      console.log(task)
+      if (!task) {
+        return res.json({ 
+            success: false, 
+            msg: "Task not found" 
+        });
+      }
+  
+      task.content = content.trim();
+      await task.save();
+  
+      return res.json({ 
+        success: true, 
+        msg: "Task updated successfully", 
+        updatedTask: task 
+    });
+    } catch (err) {
+      console.error(err);
+      return res.json({ 
+        success: false, 
+        msg: err.message 
+    });
+    }
+  });
+
 router.patch("/change_completion",verifyToken ,async(req,res)=>{
     try{
         const {task_id , completed } = req.body;
